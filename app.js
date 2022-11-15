99c4ae970b7c86461ac2886b44bc5de76f13a803
VirusZzHkP <hrisikeshpal2001@gmail.com>
2022-11-15 12:10:42 +0530

    fixed one-click location problem

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function showError(error) {
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude) {
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
 
   fetch(api)
     .then(function (response) {
